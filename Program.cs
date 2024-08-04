4a6a7e06f6bf38449cf0be23b56739b028a64925
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-08-04 15:42:34 +0200

    Looping whole program

--- Program.cs
+++ Program.cs
@@ -4,75 +4,87 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Chose what to do:");
-            Console.WriteLine("+");
-            Console.WriteLine("-");
-            Console.WriteLine("*");
-            Console.WriteLine("/");
-            char menu = Convert.ToChar(Console.ReadLine());
-            switch (menu)
+            bool validInput = false;
+            while (!validInput)
             {
-                case '+':
-                    {
-                        Console.WriteLine("Enter first number");
-                        int num1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter first number");
-                        int num2 = Convert.ToInt32(Console.ReadLine());
-                        int result = num1 + num2;
-                        Console.WriteLine($"Your result is {result}");
-                    }
-                    break;
+                Console.WriteLine("Chose what to do:");
+                Console.WriteLine("+");
+                Console.WriteLine("-");
+                Console.WriteLine("*");
+                Console.WriteLine("/");
+                char menu = Convert.ToChar(Console.ReadLine());
 
-                case '-':
-                    {
-                        Console.WriteLine("Enter first number");
-                        int num1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter first number");
-                        int num2 = Convert.ToInt32(Console.ReadLine());
-                        int result = num1 - num2;
-                        Console.WriteLine($"Your result is {result}");
-                    }
-                    break;
-                case '*':
-                    {
-                        Console.WriteLine("Enter first number");
-                        int num1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter first number");
-                        int num2 = Convert.ToInt32(Console.ReadLine());
-                        int result = num1 * num2;
-                        Console.WriteLine($"Your result is {result}");
-                    }
-                    break;
-                case '/':
-                    {
-                        Console.WriteLine("Enter first number");
-                        int num1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter first number");
-                        int num2 = Convert.ToInt32(Console.ReadLine());
-                        if (num1 == 0)
+                switch (menu)
+                {
+                    case '+':
                         {
-                            Console.WriteLine("You can not divide by 0!");
-                        }
-                        else if (num2 == 0)
-                        {
-                            Console.WriteLine("You can not divide by 0!");
-                        }
-                        else
-                        {
-                            int result = num1 / num2;
+                            Console.WriteLine("Enter first number");
+                            int num1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter first number");
+                            int num2 = Convert.ToInt32(Console.ReadLine());
+                            int result = num1 + num2;
                             Console.WriteLine($"Your result is {result}");
+                            validInput = true;
+
                         }
-                        
+                        break;
 
-                    }
-                    break;
-                default:
-                    {
-                        Console.WriteLine("Invalid operation. Please choose +, -, *, or /.");
-                    }
-                    break;
+                    case '-':
+                        {
+                            Console.WriteLine("Enter first number");
+                            int num1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter first number");
+                            int num2 = Convert.ToInt32(Console.ReadLine());
+                            int result = num1 - num2;
+                            Console.WriteLine($"Your result is {result}");
+                            validInput = true;
+                        }
+                        break;
+                    case '*':
+                        {
+                            Console.WriteLine("Enter first number");
+                            int num1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter first number");
+                            int num2 = Convert.ToInt32(Console.ReadLine());
+                            int result = num1 * num2;
+                            Console.WriteLine($"Your result is {result}");
+                            validInput = true;
+                        }
+                        break;
+                    case '/':
+                        {
+                            Console.WriteLine("Enter first number");
+                            int num1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter first number");
+                            int num2 = Convert.ToInt32(Console.ReadLine());
+                            if (num1 == 0)
+                            {
+                                Console.WriteLine("You can not divide by 0!");
+                            }
+                            else if (num2 == 0)
+                            {
+                                Console.WriteLine("You can not divide by 0!");
+                            }
+                            else
+                            {
+                                int result = num1 / num2;
+                                Console.WriteLine($"Your result is {result}");
+                            }
+                            validInput = true;
 
+
+                        }
+                        break;
+                    default:
+                        {
+
+                            Console.WriteLine("Invalid operation. Please choose +, -, *, or /.");
+                        }
+                        break;
+
+                }
             }
+
             Console.ReadKey();
         }
     }

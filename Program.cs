300cdf17fc8d31978f174ebfabf054417a865921
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-08-04 15:34:26 +0200

    Division by zero is not allowed

--- Program.cs
+++ Program.cs
@@ -49,8 +49,21 @@
                         int num1 = Convert.ToInt32(Console.ReadLine());
                         Console.WriteLine("Enter first number");
                         int num2 = Convert.ToInt32(Console.ReadLine());
-                        int result = num1 / num2;
-                        Console.WriteLine($"Your result is {result}");
+                        if (num1 == 0)
+                        {
+                            Console.WriteLine("You can not divide by 0!");
+                        }
+                        else if (num2 == 0)
+                        {
+                            Console.WriteLine("You can not divide by 0!");
+                        }
+                        else
+                        {
+                            int result = num1 / num2;
+                            Console.WriteLine($"Your result is {result}");
+                        }
+                        
+
                     }
                     break;
 

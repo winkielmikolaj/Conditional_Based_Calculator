cca944e20f947879f221fd08e7c0a6621b64d4d7
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-08-04 15:27:19 +0200

    Menu ready

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@
                     {
                         Console.WriteLine("Enter first number");
                         int num1 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter second number");
+                        Console.WriteLine("Enter first number");
                         int num2 = Convert.ToInt32(Console.ReadLine());
                         int result = num1 + num2;
                         Console.WriteLine($"Your result is {result}");
@@ -25,9 +25,31 @@
 
                 case '-':
                     {
+                        Console.WriteLine("Enter first number");
                         int num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter first number");
                         int num2 = Convert.ToInt32(Console.ReadLine());
                         int result = num1 - num2;
+                        Console.WriteLine($"Your result is {result}");
+                    }
+                    break;
+                case '*':
+                    {
+                        Console.WriteLine("Enter first number");
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter first number");
+                        int num2 = Convert.ToInt32(Console.ReadLine());
+                        int result = num1 * num2;
+                        Console.WriteLine($"Your result is {result}");
+                    }
+                    break;
+                case '/':
+                    {
+                        Console.WriteLine("Enter first number");
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter first number");
+                        int num2 = Convert.ToInt32(Console.ReadLine());
+                        int result = num1 / num2;
                         Console.WriteLine($"Your result is {result}");
                     }
                     break;
